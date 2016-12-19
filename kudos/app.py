8f5fcbb4c78343986c5355787dfe2db22e249e21
Ham Vocke <hermann.vocke@gmail.com>
2016-12-19 21:46:30 +0100

    extract logger module

--- kudos/app.py
+++ kudos/app.py
@@ -1,12 +1,11 @@
 from flask import Flask
 from flask import render_template, redirect, url_for, request, flash
+from kudos import logger
 from raven.contrib.flask import Sentry
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object('config')
 app.config.from_pyfile('config.cfg', silent=True)
-
-sentry = Sentry(app, dsn=app.config['SENTRY_DSN'])
 
 @app.route('/')
 def index():
@@ -16,5 +15,5 @@ def index():
 def create():
     app.logger.debug(request.form['email'])
     flash('Created new feedback round')
-    sentry.captureMessage('Created new feedback round')
+    logger.log('Created new feedback round')
     return redirect(url_for('index'))
--- test/api_test.py
+++ test/api_test.py
@@ -1,21 +1,24 @@
 from kudos import app
+import unittest
 
-app = app.app.test_client()
+class ApiTestCase(unittest.TestCase):
+    def setUp(self):
+        app.app.config['TESTING'] = True
+        self.kudos = app.app.test_client()
 
+    def test_should_return_200_status(self):
+        response = self.kudos.get("/")
+        assert response.status_code == 200
 
-def test_should_return_200_status():
-    response = app.get("/")
-    assert response.status_code == 200
+    def test_should_render_index_page(self):
+        response = self.kudos.get("/")
+        assert b"<h1>Kudos</h1>" in response.data
 
-def test_should_render_index_page():
-    response = app.get("/")
-    assert b"<h1>Kudos</h1>" in response.data
+    def test_should_redirect_to_index_after_create(self):
+        response = self.kudos.post("/create", data=dict(email='someMail@example.com'), follow_redirects=True)
+        assert response.status_code == 200
+        assert b"<h1>Kudos</h1>" in response.data
 
-def test_should_redirect_to_index_after_create():
-    response = app.post("/create", data=dict(email='someMail@example.com'), follow_redirects=True)
-    assert response.status_code == 200
-    assert b"<h1>Kudos</h1>" in response.data
-
-def test_should_flash_message_after_create():
-    response = app.post("/create", data=dict(email='someMail@example.com'), follow_redirects=True)
-    assert b"Created new feedback round" in response.data
+    def test_should_flash_message_after_create(self):
+        response = self.kudos.post("/create", data=dict(email='someMail@example.com'), follow_redirects=True)
+        assert b"Created new feedback round" in response.data
