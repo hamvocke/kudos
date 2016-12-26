0ff85b743b9562a7e877bb91d38429e5c49caa6e
Ham Vocke <hermann.vocke@gmail.com>
2016-12-26 21:51:26 +0100

    refactor views into method views

--- kudos/views.py
+++ kudos/views.py
@@ -1,5 +1,6 @@
 from kudos import app
 from flask import render_template, redirect, url_for, request, flash
+from flask.views import MethodView
 from kudos.models import FeedbackRound
 from kudos import db
 
@@ -7,17 +8,29 @@ from kudos import db
 def index():
     return render_template('index.html')
 
-@app.route('/create', methods=['POST'])
-def create():
-    feedbackRound = FeedbackRound('My awesome feedback session')
-    db.session.add(feedbackRound)
-    db.session.commit()
-    flash('Created new feedback round')
-    return redirect(url_for('index'))
+class FeedbackRoundApi(MethodView):
+    def get(self, name):
+        if name is None:
+            feedbackRounds = FeedbackRound.query.all()
+        else:
+            feedbackRounds = FeedbackRound.query.filter_by(name=name).all()
 
-@app.route('/feedback', methods=['GET'])
-def feedback():
-    feedbackRounds = FeedbackRound.query.all()
-    if feedbackRounds is None:
-        abort(404)
-    return render_template('feedback.html', feedbackRounds = feedbackRounds)
+        if feedbackRounds is None:
+            abort(404)
+
+        return render_template('feedbacks.html', feedbackRounds = feedbackRounds)
+
+    def post(self):
+        name = request.form['name']
+        if name is None:
+            name = 'awesome feedback'
+        feedbackRound = FeedbackRound(name)
+        db.session.add(feedbackRound)
+        db.session.commit()
+        flash('Created new feedback round')
+        return redirect(url_for('feedbackRound', name=feedbackRound.name))
+
+feedbackRoundView = FeedbackRoundApi.as_view('feedbackRound')
+app.add_url_rule('/feedbackRound/', defaults={'name': None}, view_func=feedbackRoundView, methods=['GET'])
+app.add_url_rule('/feedbackRound/', view_func=feedbackRoundView, methods=['POST'])
+app.add_url_rule('/feedbackRound/<string:name>', view_func=feedbackRoundView, methods=['GET'])
