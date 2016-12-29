494183f04379bbeeb100b770e78949a35f7e5f34
Ham Vocke <hermann.vocke@gmail.com>
2016-12-29 16:19:51 +0100

    add option sets to model

--- kudos/initial_data.py
+++ kudos/initial_data.py
@@ -5,10 +5,11 @@ from kudos import models
 def init_db():
     options = models.Option.query.all()
     if len(options) == 0:
-
         app.logger.debug("create seed data")
         like = models.Option(':)')
         dislike = models.Option(':(')
+        emoticons = models.OptionSet("Emoticons", [like, dislike])
         db.session.add(like)
         db.session.add(dislike)
+        db.session.add(emoticons)
         db.session.commit()
--- kudos/models.py
+++ kudos/models.py
@@ -1,6 +1,6 @@
 from kudos import db
 
-options = db.Table('feedback_options',
+options_feedback = db.Table('feedback_options',
                    db.Column('feedback_id', db.Integer, db.ForeignKey('feedback.id')),
                    db.Column('option_id', db.Integer, db.ForeignKey('option.id')))
 
@@ -9,7 +9,7 @@ class Feedback(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(150))
     votes = db.relationship('Vote', backref='feedback')
-    options = db.relationship('Option', secondary=options, backref=db.backref('feedbacks', lazy='dynamic'))
+    options = db.relationship('Option', secondary=options_feedback, backref=db.backref('feedbacks', lazy='dynamic'))
 
     def __init__(self, name):
         self.name = name
@@ -34,6 +34,7 @@ class Vote(db.Model):
 
 class Option(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    feedback_id = db.Column(db.Integer, db.ForeignKey('option_set.id'))
     description = db.Column(db.String(50), unique=True)
 
     def __init__(self, description):
@@ -41,3 +42,17 @@ class Option(db.Model):
 
     def __repr__(self):
         return '<Option {}>'.format(self.description)
+
+
+class OptionSet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True)
+    options = db.relationship('Option')
+
+    def __init__(self, name, options):
+        self.name = name
+        self.options = options
+
+    def __repr__(self):
+        return '<OptionSet {}>'.format(self.name)
+
