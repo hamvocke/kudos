12bd51820b083b6b750664b14823701fe42ff8a5
Ham Vocke <hermann.vocke@gmail.com>
2017-01-26 20:08:02 +0100

    add setup to app tests

--- test/app_test.py
+++ test/app_test.py
@@ -1,10 +1,15 @@
 import unittest
 from unittest.mock import patch
 
-from kudos import initial_data, db
+from kudos import app, initial_data, db
 
 
 class AppTestCase(unittest.TestCase):
+    def setUp(self):
+        app.config.from_object('config.TestingConfig')
+        self.app = app.test_client()
+        db.create_all()
+
     def tearDown(self):
         db.session.remove()
         db.drop_all()
