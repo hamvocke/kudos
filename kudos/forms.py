6bc010d49a79c7331494ff22fa96e6f43020ade1
Ham Vocke <hermann.vocke@gmail.com>
2017-01-26 20:12:30 +0100

    make options required

--- kudos/forms.py
+++ kudos/forms.py
@@ -5,7 +5,7 @@ from wtforms.validators import required, optional, Length, Email
 
 class CreateFeedbackForm(FlaskForm):
     name = StringField('Title', validators=[required(), Length(max=150)])
-    description = TextAreaField('Description', validators=[optional(), Email()])
+    description = TextAreaField('Description', validators=[optional()])
     email = StringField('E-Mail', validators=[required(), Email()])
-    options = RadioField('Answers', coerce=int)
+    options = RadioField('Answers', coerce=int, validators=[required()])
     submit = SubmitField('Create Feedback')
