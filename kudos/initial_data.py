617310d8b9cea617dd8d65fedd02d6f943c14567
Ham Vocke <hermann.vocke@gmail.com>
2017-02-01 20:22:12 +0100

    add fake data on init_db

--- kudos/initial_data.py
+++ kudos/initial_data.py
@@ -1,15 +1,22 @@
 from kudos import db, app
 from kudos import models
+from faker import Factory
+
+fake = Factory.create(locale='de_DE')
 
 
 def init_db():
-    options = models.Option.query.all()
-    if len(options) == 0:
-        app.logger.debug("create seed data")
-        like = models.Option(':)')
-        dislike = models.Option(':(')
-        emoticons = models.OptionSet("Emoticons", [like, dislike])
-        db.session.add(like)
-        db.session.add(dislike)
-        db.session.add(emoticons)
-        db.session.commit()
+    app.logger.debug("create seed data")
+    like = models.Option(':)')
+    dislike = models.Option(':(')
+    emoticons = models.OptionSet("Emoticons", [like, dislike])
+
+    db.session.add(like)
+    db.session.add(dislike)
+    db.session.add(emoticons)
+
+    for _ in range(0, 15):
+        feedback = models.Feedback(' '.join(fake.words(nb=3)), emoticons.options, fake.sentence(),
+                                   ends_at=fake.date_time_between(start_date="-5d", end_date="+5d", tzinfo=None))
+        db.session.add(feedback)
+    db.session.commit()
