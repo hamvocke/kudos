0271668e2e8ec288e5e0299d419d75eab810c3ff
Ham Vocke <hermann.vocke@gmail.com>
2017-02-12 16:38:47 +0100

    add votes to initial data

--- kudos/initial_data.py
+++ kudos/initial_data.py
@@ -1,3 +1,5 @@
+import random
+
 from kudos import db, app
 from kudos import models
 from faker import Factory
@@ -16,7 +18,19 @@ def init_db():
     db.session.add(emoticons)
 
     for _ in range(0, 15):
-        feedback = models.Feedback(' '.join(fake.words(nb=3)), emoticons.options, fake.sentence(),
+        feedback = models.Feedback(name=' '.join(fake.words(nb=3)),
+                                   options=emoticons.options,
+                                   description=fake.text(),
                                    ends_at=fake.date_time_between(start_date="-5d", end_date="+5d", tzinfo=None))
+
         db.session.add(feedback)
+        db.session.commit()
+
+        for __ in range(0, random.randint(0, 25)):
+            vote = models.Vote(feedback_id=feedback.id,
+                               option=feedback.options[random.randint(0, 1)].description,
+                               text=fake.text())
+
+            db.session.add(vote)
+
     db.session.commit()
