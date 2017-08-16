42c2902bbc3a629a08a209b0934ff0ecad1a159e
Hermann Vocke <hvocke@thoughtworks.com>
2017-08-16 17:59:04 +0200

    select options in select element

--- kudos/static/js/options.js
+++ kudos/static/js/options.js
@@ -1,19 +1,31 @@
-const options = document.querySelectorAll('.box.option');
+const optionsTarget = document.querySelectorAll(".options")[0];
+const selectedTarget = document.querySelectorAll(".selected-options")[0];
+const selectElement = document.querySelectorAll(".option-select")[0];
+
+const toggle = (text) => {
+  const options = Array.from(selectElement.getElementsByTagName('option'));
+  options
+    .filter((option) => option.innerHTML === text)
+    .map((option) => option.selected = !option.selected)
+}
 
 const selectOption = (event) => {
-  const selectedTarget = document.querySelectorAll(".selected-options")[0];
   selectedTarget.insertBefore(event.target, selectedTarget.firstChild);
   event.target.removeEventListener('click', selectOption);
   event.target.addEventListener('click', removeOption);
-}
+
+  toggle(event.target.innerHTML);
+};
 
 const removeOption = (event) => {
-  const optionsTarget = document.querySelectorAll(".options")[0];
   optionsTarget.insertBefore(event.target, optionsTarget.firstChild);
   event.target.removeEventListener('click', removeOption);
   event.target.addEventListener('click', selectOption);
+
+  toggle(event.target.innerHTML);
 };
 
+const options = document.querySelectorAll('.box.option');
 for (option of options) {
   option.addEventListener('click', selectOption);
-}
+};
