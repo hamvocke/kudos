ce146f5748afad7d13c40d3143e7654a15304103
Hermann Vocke <hvocke@thoughtworks.com>
2017-09-23 12:26:58 +0200

    add javascript for adding/removing options

--- kudos/static/js/options.js
+++ kudos/static/js/options.js
@@ -1,31 +1,42 @@
-const optionsTarget = document.querySelectorAll(".options")[0];
-const selectedTarget = document.querySelectorAll(".selected-options")[0];
-const selectElement = document.querySelectorAll(".option-select")[0];
+const optionsModule = (() => {
+  const optionsTarget = document.querySelectorAll(".options")[0];
+  const selectedTarget = document.querySelectorAll(".selected-options")[0];
+  const selectElement = document.querySelectorAll(".option-select")[0];
 
-const toggle = (text) => {
-  const options = Array.from(selectElement.getElementsByTagName('option'));
-  options
-    .filter((option) => option.innerHTML === text)
-    .map((option) => option.selected = !option.selected)
-}
+  const toggle = (text) => {
+    const options = Array.from(selectElement.getElementsByTagName('option'));
+    options
+      .filter((option) => option.innerHTML === text)
+      .map((option) => option.selected = !option.selected)
+  }
 
-const selectOption = (event) => {
-  selectedTarget.insertBefore(event.target, selectedTarget.firstChild);
-  event.target.removeEventListener('click', selectOption);
-  event.target.addEventListener('click', removeOption);
+  const selectOption = (event) => {
+    selectedTarget.insertBefore(event.target, selectedTarget.firstChild);
+    event.target.removeEventListener('click', selectOption);
+    event.target.addEventListener('click', removeOption);
 
-  toggle(event.target.innerHTML);
-};
+    toggle(event.target.innerHTML);
+  };
 
-const removeOption = (event) => {
-  optionsTarget.insertBefore(event.target, optionsTarget.firstChild);
-  event.target.removeEventListener('click', removeOption);
-  event.target.addEventListener('click', selectOption);
+  const removeOption = (event) => {
+    optionsTarget.insertBefore(event.target, optionsTarget.firstChild);
+    event.target.removeEventListener('click', removeOption);
+    event.target.addEventListener('click', selectOption);
 
-  toggle(event.target.innerHTML);
-};
+    toggle(event.target.innerHTML);
+  };
 
-const options = document.querySelectorAll('.box.option');
-for (option of options) {
-  option.addEventListener('click', selectOption);
-};
+  const init = () => {
+    const options = document.querySelectorAll('.box.option');
+    for (option of options) {
+      option.addEventListener('click', selectOption);
+    };
+  }
+
+  init();
+
+  return {
+    selectOption: selectOption,
+    removeOption: removeOption,
+  }
+})();
