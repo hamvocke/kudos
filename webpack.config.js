02b6297793de5d88158697eb88a4db82f24474f0
Hermann Vocke <hvocke@thoughtworks.com>
2017-09-23 13:19:19 +0200

    make options a ES module

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './kudos/static/js/options.js',
+  entry: './kudos/static/js/app.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'kudos/static/dist')
